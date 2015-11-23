89f635235235c09e1f39dac7e8a1c9355a07198e
Ростислав <extra0@mail.ru>
2015-11-23 17:36:28 +0300

    финальные установки шрифта

--- js/script.js
+++ js/script.js
@@ -3,4 +3,31 @@ $(function(){
 	// вызов фенсибокса
 	$('.fancybox').fancybox();
 
+	// //якорь вверх
+	// $(function() {
+	// 	$.fn.scrollToTop = function() {
+	// 		if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+	// 		var scrollDiv = $(this);
+	// 		$(window).scroll(function() {
+	// 			if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+	// 			else $(scrollDiv).fadeIn("slow")
+	// 		});
+	// 		$(this).click(function() {
+	// 			$("html, body").animate({
+	// 				scrollTop: 0
+	// 			}, "slow")
+	// 		})
+	// 	}
+	// });
+	// // вызов функции якоря
+	// $(function() {
+	// 	$(".btn-up").scrollToTop();
+	// });
+
+	$('.slider__block').bxSlider({
+		// auto: true,
+		pause: 5000,
+		pager: false
+	});
+
 });
